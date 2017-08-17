5dac078d86b55fabdc47023b5fcbd194cb599875
Christian Evenhuis <evenhuis@Christians-MacBook-Pro.local>
2017-08-17 20:27:55 +1000

    changed to python3 added codec ignore error for weird encodings cleanedup a time difference and datetime parsing

--- extract_pyro.py
+++ extract_pyro.py
@@ -1,22 +1,24 @@
 #!/usr/bin/python
-
 import re
 import time
 import fileinput
 import sys, getopt
 
-from datetime import datetime as dt
+import datetime as dt
 
 # This stops error -32 when piping into head or tail
 from signal import signal, SIGPIPE, SIG_DFL
 signal(SIGPIPE,SIG_DFL)
+
+import codecs
+sys.stdin = codecs.getreader('utf8')(sys.stdin.detach(), errors='ignore')
 
 name = ''  # this is the header
 
 try:
     opts,args = getopt.getopt(sys.argv[1:],"hc:n:st:",["help","column=","name=","string"])
 except getopt.GetoptError as err:
-    print str(err)
+    print(str(err))
     sys.exit(2)
 
 col = -1
@@ -37,12 +39,12 @@ for o, a in opts:
         t0=a
 
 if col == -1:
-    print "ERROR : columns not set "
+    print("ERROR : columns not set ")
     exit(1)
 
 
 # set the refernce time
-time0 = dt.strptime( t0,"%d/%m/%y %H:%M:%S")
+time0 = dt.datetime.strptime( t0,"%d/%m/%y %H:%M:%S")
 
 # Read a date/time from the Oxygen data
 data_found = False 
@@ -54,46 +56,23 @@ for line in fileinput.sys.stdin:
         continue
 
     if data_found : 
-        # split the date field up
-        b = re.split('/|-| +|:',a[0])
-        yr =int(b[2])
-        mon=int(b[1])
-        day=int(b[0])
-
-        b = re.split('/|-| +|:',a[1])
-        hr =int(b[0])
-        mn =int(b[1])
-        sec=int(b[2])
-
-
-        # make the time object
-        time1 = dt( yr,  mon,  day,  hr,    mn, sec )
+        time1 = dt.datetime.strptime("{} {}".format(a[0],a[1]), "%d/%m/%Y %H:%M:%S" )
 
         # calculate the difference in days
-        if( time1 < time0 ):
-            time_delta = time0 - time1
-            time = (time_delta.days*24*60*60-time_delta.seconds)/24./60./60.
-            time =  time
-        else:
-            time_delta = time1 - time0
-            time = (time_delta.days*24*60*60+time_delta.seconds)/24./60./60.
-
-        # print comment
-        #if len(a)>3 and len(a[2])>0 :
-        #    print "#",a[2]
+        time = (time1-time0)/dt.timedelta(days=1)
 
         # save the variable
         if string :
             var = a[col+1]
             var.replace("\n","")
             if( len(var)>1):
-                print '{0} "{1:s}"'.format(time,var.replace("\n","") )
+                print('{0:8.5f} "{1:s}"'.format(time,var.replace("\n","") ))
         else:
             try:
                 var = float(a[col+1])
-                print '{0} {1:.6f}'.format( time, var )
+                print('{0:8.5f} {1:.6f}'.format( time, var ))
             except:
-                print '{0}  NA'.format( time )
+                print('{0:8.5f}  NA'.format( time ))
 
     if a[0]=="Date" and a[1]=="Time (HH:MM:SS)" :
         data_found = True
